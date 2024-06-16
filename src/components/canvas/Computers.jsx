bf323c31b880c59b7d948b39a8ccdd741112c9e7
Eva Luna Álvarez <zap363@amerike.edu.mx>
2024-06-16 11:44:26 -0600

    3d model showwws its aliveeeeee

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,7 +8,7 @@ const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
       <mesh>
-        <hemisphereLight intesity={0.15} groundColor="black" />
+        <hemisphereLight intensity={0.15} groundColor="black" />
         <pointLight intensity={1} />
         <primitive object={computer.scene}/>
       </mesh>
@@ -21,10 +21,13 @@ const ComputersCanvas = () => {
         camera={{position: [20, 3, 5], fov: 25}}
         gl={{preserveDrawingBuffer : true}}>
         <Suspense fallback={<CanvasLoader />}>
-          
+            <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2}
+                minPolarAngle={Math.PI / 2}/>
+            <Computers />
         </Suspense>
+        <Preload all />
       </Canvas>
   )
 }
 
-export default Computers
+export default ComputersCanvas
