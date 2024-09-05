8ef993bdee2882b6e787654cf06a9e713218b626
Eva Luna Álvarez <zap363@amerike.edu.mx>
2024-09-04 20:58:15 -0600

    Started Tech Section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import {BallCanvas} from "./canvas";
+import {SectionWrapper} from "../hoc";
+import  {technologies} from "../constants";
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap">
+
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "");
